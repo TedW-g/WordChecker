9a8cd30654e047f369bd35fe8f3572a44fb9b495
TedW-g <tedw3@nycstudents.net>
2024-10-09 13:30:25 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> words = new ArrayList<String>();
+        WordChecker w = new WordChecker(words);
+        words.add("an");
+        words.add("band");
+        words.add("band");
+        words.add("abandon");
+        System.out.print(words);
+    }
+}
---
+++ WordChecker.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class WordChecker
+{
+    public WordChecker(ArrayList<String> list)
+    {
+        ArrayList<String> wordList = new ArrayList<String>();
+    }
+}

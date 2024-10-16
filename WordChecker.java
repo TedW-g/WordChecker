ce854f5cffb03b9f91241e2de4e2ced25efaed51
TedW-g <tedw3@nycstudents.net>
2024-10-16 13:23:17 +0000

    commit

--- WordChecker.java
+++ WordChecker.java
@@ -2,8 +2,26 @@ import java.util.ArrayList;
 
 public class WordChecker
 {
+    private ArrayList<String> wordList;
+
+    public boolean isWordChain()
+    {
+        boolean wordChain = true;
+        for (int i = 1; i < wordList.size(); i++)
+        {
+            String after = wordList.get(i);
+            String before = wordList.get(i - 1);
+            if(after.indexOf(before) < 0) return false;
+        }
+    }
+
+    public ArrayList<String> createList(String target)
+    {
+        return new ArrayList<>();
+    }
+
     public WordChecker(ArrayList<String> list)
     {
-        ArrayList<String> wordList = new ArrayList<String>();
+        wordList = new ArrayList<String>();
     }
 }
